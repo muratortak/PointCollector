a116f2dd363ed2ce5d2be564a4494f1f16802676
murat ortak <murat.ortak35@hotmail.com>
2022-11-07 23:40:37 -0500

    added first domain entitiy and auth basics

--- PointCollector/PointCollector.Application/Services/Authentication/AuthenticationService.cs
+++ PointCollector/PointCollector.Application/Services/Authentication/AuthenticationService.cs
@@ -1,25 +1,65 @@
+using PointCollector.Application.Common.Interfaces.Authentication;
+using PointCollector.Application.Common.Interfaces.Persistence;
+using PointCollector.Domain.Entities;
+
 namespace PointCollector.Application.Services.Authentication;
 
 public class AuthenticationService: IAuthenticationService 
 {
-    public AuthenticationResult Register(string FirstName, string LastName, string Email, string Password)
-    {
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+    private readonly IUserRepository _userRepository;
 
-        return new AuthenticationResult(
-            Guid.NewGuid(), 
-            FirstName, 
-            LastName, 
-            Email, 
-            "token");
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    {
+        _jwtTokenGenerator = jwtTokenGenerator;
+        _userRepository = userRepository;
     }
-    public AuthenticationResult Login(string Email, string Password)
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    {
+        if (_userRepository.GetUserByEmail(email) is not null) 
+        {
+            throw new Exception("User already exists.");
+        }
+
+        var user = new User
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            Email = email,
+            Password = password
+        };
+
+        _userRepository.Add(user);
+        // Generat Token
+        var userId = user.Id;
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+        return new AuthenticationResult(
+            userId,
+            firstName,
+            lastName,
+            email,
+            token);
+    }
+    public AuthenticationResult Login(string email, string password)
     {
 
+        if (_userRepository.GetUserByEmail(email) is not User user)
+        {
+            throw new Exception("Invalid email/password.");
+        }
+
+        if(user.Password != password)
+        {
+            throw new Exception("Invalid email/password.");
+        }
+
+        var token = _jwtTokenGenerator.GenerateToken(user.Id, user.FirstName, user.LastName);
+
         return new AuthenticationResult(
-            Guid.NewGuid(), 
-            "FirstName", 
-            "LastName", 
-            Email, 
-            "token");
+            user.Id, 
+            user.FirstName, 
+            user.LastName, 
+            email, 
+            token);
     }
 }
---
+++ PointCollector/PointCollector.Infrastructure/Authentication/JwtTokenGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using PointCollector.Application.Common.Interfaces.Authentication;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointCollector.Infrastructure.Authentication
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtSettings _jwtSettings;
+        public JwtTokenGenerator(IOptions<JwtSettings> jwtOptions)
+        {
+            _jwtSettings = jwtOptions.Value;
+        }
+        public string GenerateToken(Guid userId, string firstName, string lastName)
+        {
+            var signingCredentials = new SigningCredentials
+            (
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.Secret)),
+                SecurityAlgorithms.HmacSha256
+            );
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var securityToken = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.ExpiryMinutes),
+                claims: claims,
+                signingCredentials: signingCredentials,
+                audience: _jwtSettings.Audience
+                );
+            
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
